d680f3a7adfd7c5c08f923c5833756271e6a3ec1
Weslem Walace da Silva Lira <senhorweshell00@gmail.com>
2026-01-05 16:49:54 -0300

    Teste 4

--- servidor_teste.py
+++ servidor_teste.py
@@ -1,9 +1,36 @@
 import socket
 import threading 
 
+clientes= []
+
+def gerencia_clientes(conexao, endereco):
+    print(f"Conexão de {endereco} estabelecida.")
+    while True:
+        try:
+            dados = conexao.recv(1024)
+        except:
+            break
+        if not dados:
+            break
+        print(f"Mensagem de {endereco}: {dados.decode()}")
+
+    for c in clientes:
+        if c != conexao:
+            try:
+                c.send(dados)
+            except:
+                clientes.remove(c)
+except:
+    break
+print(f"Conexão de {endereco} encerrada.")
+clientes.remove(conexao)
+conexao.close()
+
+
+
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind(('localhost', 12345))
-servidor.listen(1)
+servidor.listen(5)
 print("servidor aguardando conexões na porta 12345...")
 
 conexao, endereco = servidor.accept()
