ce83354288debddca73f72f284fdb7d5b4495450
Weslem Walace da Silva Lira <senhorweshell00@gmail.com>
2026-01-07 15:29:42 -0300

    Teste 8

--- cliente.py
+++ cliente.py
@@ -1,32 +1,34 @@
 import socket
 import threading
 
-IP_Servidor = 'localhost'
-porta = 12345
+IP_Servidor = '0.tcp.sa.ngrok.io'
+porta = 17017
 
+nome_usuario = input("Digite seu nome: ")
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((IP_Servidor, porta)) 
+cliente.connect((IP_Servidor, porta))
+
+def enviar_mensagens():
+    while True:
+        try:
+            texto = input("Sua mensagem: ")
+            if texto:
+                mensagem_formatada = f"{nome_usuario}: {texto}"
+                cliente.send(mensagem_formatada.encode())
+        except:
+            break
 
 def receber_mensagens():
     while True:
         try:
             mensagem = cliente.recv(1024).decode()
             if mensagem:
-                print(f"\nAtualização do quadro de avisos: {mensagem}")
-                print("Digite sua mensagem: ", end="")
+                print(f"\n[Atualização]: {mensagem}")
+                print("Digite sua mensagem: ", end="", flush=True)
             else:
                 break
         except:
             print("Erro na conexão.")
-            cliente.close()
-            break
-
-def enviar_mensagens():
-    while True:
-        try:
-            msg = input("Digite sua mensagem: ")
-            cliente.send(msg.encode())
-        except:
             cliente.close()
             break
   
